d082ff702ae31398857da3d724b0417e945fe53e
Sneaky <manoonia@hotmail.com>
2020-08-03 23:04:00 +0100

    GameObject now supports method chaining

--- src/main/java/net/alevel/asteroids/engine/GameObject.java
+++ src/main/java/net/alevel/asteroids/engine/GameObject.java
@@ -33,34 +33,39 @@ public abstract class GameObject {
 		return this.rotation;
 	}
 	
-	public void setScale(float scale) {
+	public GameObject setScale(float scale) {
 		this.scale = scale;
+		return this;
 	}
 	
 	/**Note: this copies the values in the vector.
 	 * It does not reference the vector being passed (i.e. any changes made to the vector being passed will not affect the vector stored in this object)
 	 */
-	public void setPosition(Vector3f position) {
+	public GameObject setPosition(Vector3f position) {
 		this.position.set(position);
+		return this;
 	}
 	
-	public void setPosition(float x, float y, float z) {
+	public GameObject setPosition(float x, float y, float z) {
 		this.position.x = x;
 		this.position.y = y;
 		this.position.z = z;
+		return this;
 	}
 	
 	/**Note: this copies the values in the vector.
 	 * It does not reference the vector being passed (i.e. any changes made to the vector being passed will not affect the vector stored in this object)
 	 */
-	public void setRotation(Vector3f rotation) {
+	public GameObject setRotation(Vector3f rotation) {
 		this.rotation.set(rotation);
+		return this;
 	}
 	
-	public void setRotation(float x, float y, float z) {
+	public GameObject setRotation(float x, float y, float z) {
 		this.rotation.x = x;
 		this.rotation.y = y;
 		this.rotation.z = z;
+		return this;
 	}
 	
 	public Mesh getMesh() {
