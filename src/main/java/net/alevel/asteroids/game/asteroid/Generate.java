eb75b3a4464ece30b4c8e3a730e4171e340df0ce
Sneaky <manoonia@hotmail.com>
2020-08-19 16:49:18 +0100

    sphere sort of works

--- src/main/java/net/alevel/asteroids/game/asteroid/Generate.java
+++ src/main/java/net/alevel/asteroids/game/asteroid/Generate.java
@@ -1,6 +1,8 @@
 package net.alevel.asteroids.game.asteroid;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.joml.Vector3f;
 
@@ -8,63 +10,104 @@ import net.alevel.asteroids.engine.graphics.Mesh;
 
 public class Generate {
 	public static Mesh createNewModel() {
-		Vector3f[] vertices = {
-				new Vector3f(0, 0, 1),
-				new Vector3f(0, 0, -1),
-				new Vector3f(0, 1, 0),
-				new Vector3f(0, -1, 0),
-				new Vector3f(1, 0, 0),
-				new Vector3f(-1, 0, 0), //indices 0 -> 5
-		};
+		List<Vector3f> vertices = new ArrayList<Vector3f>();
+		vertices.add(new Vector3f(0, 0, 1f));
+		vertices.add(new Vector3f(0, 0, -1));
+		vertices.add(new Vector3f(0, 1, 0));
+		vertices.add(new Vector3f(0, -1, 0));
+		vertices.add(new Vector3f(1, 0, 0));
+		vertices.add(new Vector3f(-1, 0, 0)); //indices 0 -> 5
 		
+		List<int[]> indices = new ArrayList<int[]>();
+		indices.add(new int[] {0, 2, 4});
+		indices.add(new int[] {0, 3, 4});
+		indices.add(new int[] {1, 3, 5});
+		indices.add(new int[] {1, 5, 2});
+		indices.add(new int[] {2, 1, 4});
+		indices.add(new int[] {3, 1, 4});
+		indices.add(new int[] {5, 0, 2});
+		indices.add(new int[] {5, 0, 3});
 		
+		for(int i = 0; i < 6; i++)
+			splitTriangles(vertices, indices);
+		for(int i = 0; i < vertices.size(); i++)
+			vertices.set(i, adjustVertexToRadius(vertices.get(i), 1));
 		
-		Vector3f[] midPoints = {
-				getMidpoint(vertices[0], vertices[2]),
-				getMidpoint(vertices[2], vertices[4]),
-				getMidpoint(vertices[4], vertices[0]),
-				
-				getMidpoint(vertices[0], vertices[3]),
-				getMidpoint(vertices[3], vertices[4]),
-				getMidpoint(vertices[4], vertices[0]),
-		};
-		
-		float[] floats = new float[(vertices.length + midPoints.length) * 3];
-		for(int i = 0; i < vertices.length; i++) {
-			floats[i * 3] = vertices[i].x;
-			floats[i * 3 + 1] = vertices[i].y;
-			floats[i * 3 + 2] = vertices[i].z;
+		float[] floats = new float[vertices.size() * 3];
+		int[] ints = new int[indices.size() * 3];
+		for(int i = 0; i < vertices.size(); i++) {
+			floats[i * 3] = vertices.get(i).x;
+			floats[i * 3 + 1] = vertices.get(i).y;
+			floats[i * 3 + 2] = vertices.get(i).z;
 		}
-		for(int i = 0; i < midPoints.length; i++) {
-			floats[(i * 3) + (vertices.length * 3)] = midPoints[i].x;
-			floats[(i * 3) + (vertices.length * 3) + 1] = midPoints[i].y;
-			floats[(i * 3) + (vertices.length * 3) + 2] = midPoints[i].z;
+		for(int i = 0; i < indices.size(); i++) {
+			int[] triangle = indices.get(i);
+			ints[i * 3] = triangle[0];
+			ints[i * 3 + 1] = triangle[1];
+			ints[i * 3 + 2] = triangle[2];
 		}
+		
 		System.out.println(Arrays.toString(floats));
-		System.out.println(Arrays.toString(midPoints));
+		System.out.println(Arrays.toString(ints));
+		return new Mesh(floats, floats, floats, ints);
+	}
+	
+	private static void getMidpointTriangle(int[] selectedVertices, List<Vector3f> vertices, List<int[]> indices) {
+		Vector3f[] midpoints = new Vector3f[3];
+		midpoints[0] = getMidpoint(vertices.get(selectedVertices[0]), vertices.get(selectedVertices[1]));
+		midpoints[1] = getMidpoint(vertices.get(selectedVertices[1]), vertices.get(selectedVertices[2]));
+		midpoints[2] = getMidpoint(vertices.get(selectedVertices[2]), vertices.get(selectedVertices[0]));
+		int firstIndex = vertices.size();
+		vertices.addAll(Arrays.asList(midpoints));
+		List<int[]> newTriangles = new ArrayList<int[]>();
+		newTriangles.add(new int[] {firstIndex, firstIndex + 1, firstIndex + 2});
+		newTriangles.add(new int[] {firstIndex, selectedVertices[0], firstIndex + 2});
+		newTriangles.add(new int[] {firstIndex, selectedVertices[1], firstIndex + 1});
+		newTriangles.add(new int[] {firstIndex + 1, selectedVertices[2], firstIndex + 2});
+		indices.remove(selectedVertices);
+		indices.addAll(newTriangles);
+	}
+	
+	private static void splitTriangles(List<Vector3f> vertices, List<int[]> indices) {
+		@SuppressWarnings("unchecked")
+		ArrayList<int[]> indicesCopy = (ArrayList<int[]>) ((ArrayList<int[]>) indices).clone();
+		System.out.println(indicesCopy);
+		for(int[] i : indicesCopy)
+			getMidpointTriangle(i, vertices, indices);
+	}
+	
+	private static Vector3f adjustVertexToRadius(Vector3f vertex, float radius) {
+		//Vector3f translateBy = new Vector3f();
+		//float angleToX = vertex.x == 0 ? vertex.z : vertex.z / vertex.x;
+		//float angleToY = vertex.x == 0 ? vertex.y : vertex.y / vertex.x;
+		//float angleToZ = vertex.z == 0 ? vertex.x : vertex.x / vertex.z;
+		float angleToX = getAngle(vertex.z, vertex.x);
+		float angleToY = getAngle(vertex.y, vertex.x);
+		return new Vector3f(radius, 0, 0).rotateY(angleToX).rotateX(angleToY);
+		//float scale = vertex.length() / radius;
+		//Vector3f out = new Vector3f(radius * angleToX, radius * angleToY, radius * angleToZ);
 		
-		Mesh mesh = new Mesh(floats, floats, floats, new int[] {
-			0, 2, 4,
-			0, 3, 4,
-			1, 3, 5,
-			1, 5, 2,
-			2, 1, 4,
-			3, 1, 4,
-			5, 0, 2,
-			5, 0, 3,
-			
-			6, 7, 8,
-			9, 10, 11
-		});
-		
-		return mesh;
+		//return vertex.mul(scale);
+		//float angleToZ = getAngle(vertex.x, vertex.z);
+		//System.out.println(angleToX + " - " + angleToY + " - " + angleToZ);
+		//float angle = (float) Math.acos(vertex.dot(new Vector3f(0, 0, 0)) / vertex.distance(0, 0, 0));
+		//return new Vector3f((float) Math.cos(angleToX), (float) Math.cos(angleToY), (float) Math.cos(angleToZ)).mul(radius).mul(angle);
+	}
+	
+	private static float getAngle(float opp, float adj) {
+		float angleToAdd;
+		if(adj < 0 && opp < 0)
+			angleToAdd = (float) Math.PI;
+		else if(adj < 0 && opp >= 0)
+			angleToAdd = (float) Math.PI;
+		else if(adj >= 0 && opp < 0)
+			angleToAdd = (float) Math.PI * 2;
+		else
+			angleToAdd = 0;
+		return angleToAdd + ((float) Math.atan(opp / adj));
 	}
 	
 	private static Vector3f getMidpoint(Vector3f a, Vector3f b) {
 		return new Vector3f(a).add(b).div(2);
 	}
 }
-
-//new Vector3f((float) Math.sqrt(1) / 2, 0, (float) Math.sqrt(1) / 2),
-//new Vector3f(0, (float) Math.sqrt(1) / 2, (float) Math.sqrt(1) / 2),
-//new Vector3f((float) Math.sqrt(1) / 2, (float) Math.sqrt(1) / 2, 0),
